54382431a2cd0485aa403bd0129f310b286f33a5
nanguage <nanguage@yahoo.com>
2023-01-18 19:03:41 +0800

    add middle mouse drag to view

--- easynode/graph_view.py
+++ easynode/graph_view.py
@@ -20,3 +20,33 @@ class GraphView(QtWidgets.QGraphicsView):
         self.setViewportUpdateMode(QtWidgets.QGraphicsView.FullViewportUpdate)
         self.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
         self.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+
+    def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:
+        if event.button() == QtCore.Qt.MiddleButton:
+            self.middleMouseButtonPress(event)
+        else:
+            super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event: QtGui.QMouseEvent) -> None:
+        if event.button() == QtCore.Qt.MiddleButton:
+            self.middleMouseButtonRelease(event)
+        else:
+            return super().mouseReleaseEvent(event)
+
+    def middleMouseButtonPress(self, event: QtGui.QMouseEvent):
+        fake_release_midele = QtGui.QMouseEvent(
+            QtCore.QEvent.MouseButtonRelease, event.localPos(), event.screenPos(),
+            QtCore.Qt.MiddleButton, QtCore.Qt.NoButton, event.modifiers())
+        super().mouseReleaseEvent(fake_release_midele)
+        self.setDragMode(QtWidgets.QGraphicsView.ScrollHandDrag)
+        fake_press_left = QtGui.QMouseEvent(
+            event.type(), event.localPos(), event.screenPos(),
+            QtCore.Qt.LeftButton, QtCore.Qt.NoButton, event.modifiers())
+        super().mousePressEvent(fake_press_left)
+
+    def middleMouseButtonRelease(self, event: QtGui.QMouseEvent):
+        fake_release_left = QtGui.QMouseEvent(
+            QtCore.QEvent.MouseButtonRelease, event.localPos(), event.screenPos(),
+            QtCore.Qt.LeftButton, QtCore.Qt.NoButton, event.modifiers())
+        super().mouseReleaseEvent(fake_release_left)
+        self.setDragMode(QtWidgets.QGraphicsView.NoDrag)
