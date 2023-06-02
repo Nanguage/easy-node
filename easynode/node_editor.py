d0b5459dd4c32ba6f9c06e6b7c85b696c89d04d3
Nanguage <Nanguage@yahoo.com>
2023-06-02 11:25:09 +0800

    add tabs

--- easynode/node_editor.py
+++ easynode/node_editor.py
@@ -6,6 +6,7 @@ from .graphics.scene import GraphicsScene
 from .graphics.view import GraphicsView
 from .setting import EditorSetting
 from .node_factory import NodeFactoryTable
+from .widgets.custom_tab import CustomTabWidget
 
 
 class NodeEditor(QtWidgets.QWidget):
@@ -19,28 +20,56 @@ class NodeEditor(QtWidgets.QWidget):
         if setting is None:
             setting = EditorSetting()
         self.setting = setting
+        self.factory_table = NodeFactoryTable()
+        self.scenes: T.List[GraphicsScene] = []
+        self.views: T.List[GraphicsView] = []
+        self.current_view: T.Optional[GraphicsView] = None
         self.init_layout()
         self.load_style_sheet(style_sheet)
-        self.factory_table = NodeFactoryTable()
+
+    @property
+    def current_scene(self) -> GraphicsScene:
+        if self.current_view is None:
+            raise ValueError("No current view")
+        else:
+            return self.current_view.scene()
 
     def create_node(self, type_name: str):
         factory = self.factory_table.table.get(type_name)
         if factory is None:
             raise ValueError(f"Node type {type_name} not found")
         node = factory.create_node()
-        self.scene.graph.add_node(node)
+        self.current_scene.graph.add_node(node)
 
     def init_layout(self):
         self.resize(800, 600)
         self.setWindowTitle("EasyNode")
 
-        self.scene = GraphicsScene(self)
-        self.scene.editor = self
+        self.tabs = CustomTabWidget(self)
+        self.tabs.add_btn.clicked.connect(self.add_scene_and_view)
+        self.tabs.tabCloseRequested.connect(self.delete_tab)
+        self.add_scene_and_view()
 
         self.layout = QtWidgets.QVBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
-        self.view = GraphicsView(self.scene, self)
-        self.layout.addWidget(self.view)
+        self.layout.addWidget(self.tabs)
+
+    def add_scene_and_view(self):
+        scene = GraphicsScene(self)
+        scene.editor = self
+        view = GraphicsView(scene, self)
+        self.scenes.append(scene)
+        self.views.append(view)
+        new_scene_idx = len(self.scenes)
+        self.tabs.addTab(view, f"scene {new_scene_idx}")
+        new_view_idx = len(self.views)
+        self.tabs.setCurrentIndex(new_view_idx)
+        self.current_view = view
+
+    def delete_tab(self, index: int):
+        self.tabs.removeTab(index)
+        view = self.views.pop(index)
+        self.scenes.remove(view.scene())
 
     def load_style_sheet(self, style_sheet: T.Optional[str] = None):
         app = QtWidgets.QApplication.instance()
---
+++ easynode/widgets/custom_tab.py
@@ -0,0 +1,87 @@
+"""
+reference:
+    https://stackoverflow.com/a/8718039/8500469
+"""
+
+from qtpy import QtWidgets
+from qtpy import QtCore
+
+
+class TabBar(QtWidgets.QTabBar):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self._editor = QtWidgets.QLineEdit(self)
+        self._editor.setWindowFlags(QtCore.Qt.Popup)
+        self._editor.setFocusProxy(self)
+        self._editor.editingFinished.connect(self.handleEditingFinished)
+        self._editor.installEventFilter(self)
+
+    def eventFilter(self, widget, event):
+        if ((event.type() == QtCore.QEvent.MouseButtonPress and
+             not self._editor.geometry().contains(event.globalPos())) or
+            (event.type() == QtCore.QEvent.KeyPress and
+             event.key() == QtCore.Qt.Key_Escape)):
+            self._editor.hide()
+            return True
+        return super().eventFilter(widget, event)
+
+    def mouseDoubleClickEvent(self, event):
+        index = self.tabAt(event.pos())
+        if index >= 0:
+            self.editTab(index)
+
+    def editTab(self, index):
+        rect = self.tabRect(index)
+        self._editor.setFixedSize(rect.size())
+        add_btn = self.parent().add_btn
+        add_btn_width = add_btn.rect().width()
+        new_point = self.parent().mapToGlobal(rect.topLeft())
+        new_point.setX(new_point.x() + add_btn_width)
+        self._editor.move(new_point)
+        self._editor.setText(self.tabText(index))
+        if not self._editor.isVisible():
+            self._editor.show()
+
+    def handleEditingFinished(self):
+        index = self.currentIndex()
+        if index >= 0:
+            self._editor.hide()
+            self.setTabText(index, self._editor.text())
+
+
+tab_widget_style = """
+QWidget {
+    font-size: 16px;
+}
+
+QPushButton {
+    font-size: 20px;
+    font-weight: bold;
+}
+"""
+
+
+class CustomTabWidget(QtWidgets.QTabWidget):
+    def __init__(self, parent=None, min_closeable=1):
+        super().__init__(parent=parent)
+        self.min_closeable = min_closeable
+        self.setStyleSheet(tab_widget_style)
+        self.tab_bar = TabBar(self)
+        self.setTabBar(self.tab_bar)
+        self.add_btn = QtWidgets.QPushButton("+")
+        self.add_btn.setToolTip("Add script")
+        self.setCornerWidget(self.add_btn, QtCore.Qt.Corner.TopLeftCorner)
+        self.setMovable(False)
+        self.setTabsClosable(False)
+
+    def removeTab(self, index):
+        super().removeTab(index)
+        if self.count() <= self.min_closeable:
+            self.setTabsClosable(False)
+            self.setMovable(False)
+
+    def addTab(self, *args, **kwargs):
+        if self.count() >= 1:
+            self.setTabsClosable(True)
+            self.setMovable(True)
+        return super().addTab(*args, **kwargs)
